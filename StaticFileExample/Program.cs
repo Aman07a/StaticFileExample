dff728eabd1fb9b485e81ea5256ed5b2f52c953d
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-24 09:22:16 +0100

    WebRoot and UseStaticFiles

--- StaticFileExample/Program.cs
+++ StaticFileExample/Program.cs
@@ -1,8 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+{
+    WebRootPath = "myroot"
+});
+
 var app = builder.Build();
 
+// Works the web root path (myroot)
 app.UseStaticFiles();
 
+// Works with "mywebroot"
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "mywebroot"))
+});
+
+// C:\StaticFileExample\StaticFileExample
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
